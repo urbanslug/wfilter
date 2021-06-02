d08fea642613b0e090f92e85ec4cc7a10986bfb2
Njagi Mwaniki <njagi@urbanslug.com>
2021-06-02 23:02:39 +0300

    Add the main wflambda fn Does not compile

---
+++ src/wflambda/affine_wavefront_utils.rs
@@ -0,0 +1,6 @@
+use super::types::wavefront::AffineWavefronts;
+
+/*
+ * Initial Conditions and finalization
+ */
+pub fn affine_wavefront_initialize(affine_wavefronts: &AffineWavefronts) {}
--- src/wflambda/mod.rs
+++ src/wflambda/mod.rs
@@ -1,1 +1,4 @@
 pub mod types;
+pub mod affine_wavefront_align;
+pub mod affine_wavefront_utils;
+pub mod affine_wavefront_extend;

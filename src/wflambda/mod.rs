2d240d5c4d4c4f5a65f1465df30a734d618174f5
Njagi Mwaniki <njagi@urbanslug.com>
2021-06-18 20:58:32 +0300

    Start on wflambda from the bottom up

--- src/wflambda/mod.rs
+++ src/wflambda/mod.rs
@@ -1,9 +1,1 @@
-//pub mod gap_affine::affine_wavefront_extend;
-// pub mod affine_wavefront_utils;
-//pub mod align_wavefront_align;
-
-// re-export as lib
-// pub use align_wavefront_align as lib;
-
-pub mod edit;
-pub mod gap_affine;
+pub mod wflambda;
